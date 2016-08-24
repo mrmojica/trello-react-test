4675731187324438b420a4447834151b220e20d1
yolixtly <yolixtly_13@hotmail.com>
2016-08-24 11:07:38 -0400

    starting fresh

--- client/js/index.js
+++ client/js/index.js
@@ -1,66 +1,86 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+//step 4 : build the card 
 var Card = function(props){
-	// Card.defaultProps = {
-	// 		message : "This is a default message"
-	// 	}
-	return (
-
-		<div className = "card">
-			<div className="card-message">{props.message}</div>
-		</div>
-
-	);
+    return (
+         <div className="card">
+            {props.text} 
+        </div>
+    );
+};
+// Step 3 : Build the lists that has title and has content dynamic
+var List = function(props){
+    var cardsArr = [];
+    var myCards = props.cards.map(function(val, index){
+            for(var i = 0; i<= props.cards.length; i++){
+             return(<Card text={props.cards[i].listCards[index]} />);
+            }
+    });
+        // var cardsArr = [];
+        // for(var i = 0; i<= 3; i++){
+        //     for(var j=0; j<= 3; j++){
+        //       cardsArr.push(<Card text={data[i].listCards[j]} />);
+        //     }
+        //     }
+    //  var myCards = state.cards.map(function(val, index){
+    //     for(var i = 0; i<= 3; i++){
+    //       return <Card text={state.cards[i].listCards[index]} />;
+    //     }
+    // });
+    return (
+        <div className="list">
+         <div className="list-title">{props.title}</div>
+        {myCards}
+        </div>
+    );
 };
 
-var trello = ['france', 'Usa', 'mexico', 'colombia']
-var cities = ['York', 'New York', 'Baltimore', 'Washington']
-var all = {trello, cities};
+//STEP 2 : build the Board woth title and with lists
 
-// var List2 = function(props) {
-// 	var cards 
-// }
+var Board = function(props){
+    var myLists = props.lists.map(function(val, index){
+        return <List title={props.lists[index].listTitle} cards={data} />
+    });
+   //  var myLists = [];
+   // for (var i=0; i<props.lists.length; i++) {
+   //      myLists.push(<List title={props.lists[i].listTitle} cards={props.lists[i].listCards} />);
+   //  }
+    return (
+        <div className="Board">
+        <div className="board-title">
+        {props.title}
+          </div>
+        <div className="board-list">{myLists}</div>
+        </div>
+
+    );
+};
+
+
+var data = [
+    {
+        "listTitle" : "Autos",
+        "listNumber" : 0,
+        "listCards" : ["Honda", "BMW", "Porsche"]
+    },
+    {
+       "listTitle" : "Fruit",
+        "listNumber" : 1,
+        "listCards" : ["apple", "banana", "berries"]
+    },
+    {
+        "listTitle" : "Countries",
+        "listNumber" : 2,
+        "listCards" : ["Mexico", "China", "USA"]
+    }
+];
 
 
 
-var List = React.createClass({
-	render: function() {
- 		var messages = [];
-		for(var i = 0; i <= trello.length; i++) {
-			messages.push(<Card message={trello[i]} />);	
-		};		
 
-
-		return (
-			<div className="list">
-			<div className="list-title" titleClass="orange">{this.props.title}</div>
-
- 				{messages}
- 			
-
-			</div>				
-		);
-	}
-});
-var title = ['Countries', 'Cities', 'States' ];
-var Board = React.createClass({
-	render: function() {
-		var list = [];
-		for(var i = 0; i < this.props.everything.length; i++) {
-			list.push(<List title={this.props.everything[i]} />);
-		};
-		return (
-			<div className="board">
-	
-				{list}
-			
-			</div>
-		);
-	}
-});
 
 
 document.addEventListener('DOMContentLoaded', function() {
-    ReactDOM.render(<Board everything={title} />, document.getElementById('app'));
+    ReactDOM.render(<Board title="Hello Board!" lists={data}/> , document.getElementById('app'));
 });
